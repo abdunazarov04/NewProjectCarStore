6fa2643aeed5b21b82785f4e48a19077d3cdebbc
Xusniddin <muxiddinovxusniddin34@gmail.com>
2023-12-25 17:22:45 +0500

    main showmenu() method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        showMenu();
     }
+
+    private static void showMenu() {
+        System.out.println("""
+                1. login
+                2. register
+                """);
+    }
+
+
 }
